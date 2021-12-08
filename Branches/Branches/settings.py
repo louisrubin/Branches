46ebdf3dd6099cdba07725ac6d7255b832bf9406
louisrubin <miguel10.rbn@gmail.com>
2021-12-07 21:57:00 -0300

    commit 2

--- Branches/Branches/settings.py
+++ Branches/Branches/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Branches.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'branches_db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
