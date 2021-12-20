c6f12e6b99dd289e5f590022d37c9398841388c7
louisrubin <miguel10.rbn@gmail.com>
2021-12-20 13:03:35 -0300

    admin-ver-perfil

--- Branches/apps/usuarios/views.py
+++ Branches/apps/usuarios/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db                  import models
 from django.shortcuts           import render
 from django.urls                import reverse_lazy
+from django.views.generic.base  import View
 from django.views.generic       import ListView, CreateView
 from django.views.generic.edit  import DeleteView, UpdateView, DeleteView
 from  apps.core.mixins import AdminRequiredMixins
@@ -13,26 +14,50 @@ from .models import Usuario
 def usuarios(request):
     return render(request, 'usuarios/usuarios.html')
 
+
+class PerfilUsuario(LoginRequiredMixin, ListView):
+    template_name = "usuarios/perfil.html"
+    model = Usuario
+    form_class = UsuarioForm
+
+    context_object_name = "usuario_data"
+
+
 class ListarAdmin(LoginRequiredMixin, AdminRequiredMixins, ListView):
     template_name = "usuarios/admin/listar.html"
     model = Usuario
     context_object_name = "usuarios"
     paginate_by = 10
 
-    # return de query + filter
     def get_queryset(self):
-        # self.request
         return Usuario.objects.all().order_by("id")
-    
-"""
-class Nuevo_only_Admin(CreateView):
-    template_name= "usuarios/admin/nuevo.html"
-    model = Usuario
-    form_class = UsuarioForm
 
-    def get_success_url(self, **kwargs) -> str:
-        return reverse_lazy("usuarios:admin_listar")
-"""
+
+class Ver_Perfil_Admin(AdminRequiredMixins, View):
+    # template_name = "usuarios/admin/admin-ver-perfil.html"    # url: admin_ver_perfil
+    # model = Usuario
+    # form_class = UsuarioForm
+    # context_object_name = "usuario"
+
+    
+    def get(self, request, pk, *args, **kwargs):
+        perfil = Usuario.objects.get(pk = pk)
+
+        context = {
+            'perfil': perfil,
+        }
+        return render(request, 'usuarios/admin/admin-ver-perfil.html', context) 
+
+
+    def post(self, request, pk, *args, **kwargs):
+        perfil = Usuario.objects.get(pk = pk)
+
+        context = {
+            'perfil': perfil,
+        }
+        return render(request, 'usuarios/admin/admin-ver-perfil.html', context)
+
+
 
 
 class Editar_only_Admin(AdminRequiredMixins, UpdateView):
@@ -51,13 +76,6 @@ class RegistroUsuario(CreateView):
 
     success_url = reverse_lazy("login")
 
-
-class PerfilUsuario(LoginRequiredMixin, ListView):
-    template_name = "usuarios/perfil.html"
-    model = Usuario
-    form_class = UsuarioForm
-
-    context_object_name = "usuario_data"
 
 
 class Delete_User(AdminRequiredMixins, DeleteView):
